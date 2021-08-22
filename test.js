e2702dab1b14145b3f04cccb11cf9bbfc35e6567
yd160513 <1223519172@qq.com>
2021-08-22 09:39:43 +0800

    学习选择排序

--- test.js
+++ test.js
@@ -51,17 +51,85 @@
 // console.log(search(list, otherTarget))
 
 // 冒泡排序 ---------------------------------------------------------------------------
-function bubbleSort(list) {
-  for (let i = 0; i < list.length - 1; i++) {
-     for (let k = 0; k < list.length - 1 - i; k++) {
-       if (list[k] > list[k + 1]) {
-          const temp = list[k]
-          list[k] = list[k + 1]
-          list[k + 1] = temp
-       }
-     }
+// /**
+//  * 冒泡排序
+//  * @param {array} list 无序的数字数组
+//  * @returns list
+//  * 1. 数组中的第一个和第二个比较，如果第二个比第一个大则两个交换位置，反之顺序保持不变。
+//  * 2. 数组的第二个和第三个比较，规则同 1。
+//  * 3. 一直比较到数组的最后一个。
+//  * 4. 这个时候数组的最后一个肯定是最大的。
+//  * 5. 然后再从 1 开始循环比较。
+//  * 6. 再次开始比较的时候因为最后一个已经是最大的了，所以就不需要再次比较了。第一次是最后一个不需要比较，第二次是最后两个不需要比较...
+//  */
+//  function bubbleSort(list) {
+//   // 一共有多少个元素需要比较，也就是一共要对比多少次
+//   for (let j = 0; j < list.length - 1; j++) {
+//     // 外层 for 循环只是告诉有多少个元素要比较，
+//     // 这里的 for 循环开始比较，
+//     // - j 的原因是：
+//     //    当第一次比较完成之后，数组的最后一个肯定是最大的了，所以在下次就不需要比对最后一个了，
+//     //    第一次是最后一个不需要比较，第二次是最后两个不需要比较...
+//     //    外层循环的次数就是已经比对过的次数，也就是数组后边不需要比对的个数，所以将其减去
+//     for (let k = 0; k < list.length - 1 - j; k++) {
+//       // 如果第一个值比第二个值大的话则交换位置，反之顺序保持不变
+//       if (list[k] > list[k + 1]) {
+//         const temp = list[k]
+//         list[k] = list[k + 1]
+//         list[k + 1] = temp
+//       } 
+//     }
+//   }
+//   return list
+// }
+// const arr = [2, 8, 4, 5, 9, 0, 1]
+// console.log(test(arr))
+
+// 选择排序 ---------------------------------------------------------------------------
+/**
+ * 选择排序
+ * @param {array} list 无序数组
+ * @returns list
+ * 
+ * 假设第一个元素是当前数组中最小的，将其索引存起来
+ * 继续遍历后边的元素，如果有其他元素小于记下的这个元素的话则将记下的元素索引替换为这个元素的索引，
+ * 遍历完成一遍之后将记下的这个索引对应的元素替换至数组第一位，遍历完成第二遍后，将记下的元素替换值数组第二位，
+ * 也就是说每遍历完成第几遍，替换的位置就是第几个
+ */
+ function test(list) {
+  let cacheIndex
+  const len = list.length
+  /**
+   * 问: 为什么外层循环的结束条件是 i < len - 1 而不是 i < len ?
+   * 答: 这里也可以写成 i < len，但是 - 1 的原因是可以减少最后一次的无用循环。
+   *    [2, 8, 4, 5, 9, 0, 1] 这么一个数组
+   *    他的外层满足条件的最后一次循环是 i = 6，由于内层循环的起始值是 i + 1，
+   *    也就导致了外层的这次循环根本就进入不了内层循环，因为 6 + 1 不小于 len，
+   *    也正因为最后一次什么都没做，倒数第二次的结果其实是和最后一次的结果是一样的，
+   *    所以也就可以让外层循环少做一次循环，也就是把 i < len 改为 i < len - 1
+   */
+  for (let i = 0; i < len - 1; i++) {
+    console.log(`外层索引为${i}的循环`)
+    cacheIndex = i
+    for (let k = i + 1; k < len; k++) {
+      console.log(`内层索引为${k}的循环`)
+      const iVal = list[cacheIndex]
+      const kVal = list[k]
+      if (iVal > kVal) {
+        cacheIndex = k
+      }
+    }
+    // 当记下的值小于 i 的值时进行替换
+    if (list[cacheIndex] < list[i]) {
+      const temp = list[i]
+      list[i] = list[cacheIndex]
+      list[cacheIndex] = temp
+    }
+    
+    console.log(`外层索引为${i}的循环后的 list =>`, list)
   }
   return list
 }
+
 const arr = [2, 8, 4, 5, 9, 0, 1]
-console.log(bubbleSort(arr))
+console.log(test(arr))
